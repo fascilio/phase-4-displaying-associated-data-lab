4109f7fdceb9bff33db24a1e332cb8a480f394a4
fascilio <fascilior@gmail.com>
2023-06-27 11:22:18 +0300

    created controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items.as_json(only: [:id, :name, :description, :price], include: { user: { only: [:id, :username, :city] } })
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: {
+          id: user.id,
+          username: user.username,
+          city: user.city,
+          items: user.items.as_json(only: [:id, :name, :description, :price])
+        }
+    end
 end
